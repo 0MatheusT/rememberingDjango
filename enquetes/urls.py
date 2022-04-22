0c82e13c1453b1c677f604b3206be1be172d442a
Matheus Tavares <matheustavares1@live.com>
2022-04-22 17:43:54 -0300

    Using POST Method to making changes on Database

--- enquetes/urls.py
+++ enquetes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "enquetes"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:id_enquete>/", views.detalhes, name="detalhes"),
--- enquetes/views.py
+++ enquetes/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
 # from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
-from .models import Pergunta
+from .models import Pergunta, Escolha
 
 
 # def index(request):
@@ -39,10 +40,40 @@ def detalhes(request, id_enquete):
     return render(request, "enquetes/detalhes.html", {"pergunta": pergunta})
 
 
-def resultados(request, id_enquete):
+"""def resultados(request, id_enquete):
     resposta = "Você está olhando os resultados da enquete: %s."
     return HttpResponse(resposta % id_enquete)
+"""
+
+
+def resultados(request, id_enquete):
+    pergunta = get_object_or_404(Pergunta, pk=id_enquete)
+    return render(request, "enquetes/resultados.html", {"pergunta": pergunta})
+
+
+"""def votar(request, id_enquete):
+    return HttpResponse("Você está votando na seguinte enquete: %s." % id_enquete)"""
 
 
 def votar(request, id_enquete):
-    return HttpResponse("Você está votando na seguinte enquete: %s." % id_enquete)
+    pergunta = get_object_or_404(Pergunta, pk=id_enquete)
+    try:
+        selected_choice = pergunta.escolha_set.get(pk=request.POST["escolha"])
+    except (KeyError, Escolha.DoesNotExist):
+
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "enquetes/detalhes.html",
+            {
+                "pergunta": pergunta,
+                "error_message": "Você não selecionou uma resposta.",
+            },
+        )
+    else:
+        selected_choice.votos += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("enquetes:resultados", args=(pergunta.id,)))
